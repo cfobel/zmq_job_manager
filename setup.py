9c8eec8b4726073068eb435012f7112ff437756c
Christian Fobel <christian@fobel.net>
2013-03-26 19:27:23 -0400

    Add setup.py to allow installation using pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'zmq_job_manager',
+    version = '0.0.1',
+    description = 'ZeroMQ job manager',
+    keywords = 'zmq job manager cluster',
+    author = 'Christian Fobel',
+    url = 'https://github.com/cfobel/zmq_job_manager',
+    license = 'GPL',
+    long_description = '''''',
+    packages = ['zmq_job_manager'],
+)

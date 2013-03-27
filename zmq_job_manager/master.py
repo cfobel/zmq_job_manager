cd51b59ca0e1cc3d9484d710b091032b46777ab1
Christian Fobel <christian@fobel.net>
2013-03-27 19:25:43 -0400

    [WIP] Show proof-of-concept multi-frame messages

--- zmq_job_manager/master.py
+++ zmq_job_manager/master.py
@@ -1,15 +1,36 @@
-from zmq_helpers.socket_configs import EchoServer
+from collections import OrderedDict
+
+import zmq
+from zmq.utils import jsonapi
+from zmq_helpers.socket_configs import SockConfigsTask, DeferredSocket
 
 
-class Master(EchoServer):
-    pass
+class Master(SockConfigsTask):
+    def __init__(self, rep_uri, control_pipe=None, **kwargs):
+        self.uris = OrderedDict([
+            ('rep', rep_uri),
+        ])
+        self.sock_configs = OrderedDict([
+                ('rep', DeferredSocket(zmq.REP)
+                            .stream_callback('on_recv', self.process_request)),
+        ])
+        for k in self.sock_configs:
+            if kwargs.get(k + '_bind'):
+                self.sock_configs[k].bind(self.uris[k])
+            else:
+                self.sock_configs[k].connect(self.uris[k])
+        super(Master, self).__init__(self.sock_configs, control_pipe=control_pipe)
+
+    def process_request(self, env, multipart_message):
+        timestamp, command, args, kwargs = map(jsonapi.loads, multipart_message)
+        env['socks']['rep'].send_multipart(multipart_message)
 
 
 def parse_args():
     """Parses arguments, returns (options, args)."""
     from argparse import ArgumentParser
 
-    parser = ArgumentParser(description='''Router-to dealer broker''')
+    parser = ArgumentParser(description='''Job manager master''')
     parser.add_argument(nargs=1, dest='rep_uri', type=str)
     args = parser.parse_args()
     args.rep_uri = args.rep_uri[0]
--- zmq_job_manager/worker.py
+++ zmq_job_manager/worker.py
@@ -2,22 +2,27 @@ from collections import OrderedDict
 from datetime import datetime
 
 import zmq
+from zmq.utils import jsonapi
 
 
 class Worker(object):
-    def __init__(self, master_uri, master_bind=False, control_pipe=None):
+    def __init__(self, master_uri, master_bind=False, time_limit='5m',
+                 memory_limit='1G', n_procs=1, n_threads=1, control_pipe=None):
         self.uris = OrderedDict(master=master_uri)
         self.binds = OrderedDict(master=master_bind)
+        self.config = dict(time_limit=time_limit, memory_limit=memory_limit,
+                           n_procs=n_procs, n_threads=n_threads)
 
     def run(self):
         ctx = zmq.Context.instance()
-        sock = zmq.Socket(ctx, zmq.DEALER)
+        sock = zmq.Socket(ctx, zmq.REQ)
         if self.binds['master']:
             sock.bind(self.uris['master'])
         else:
             sock.connect(self.uris['master'])
-        for i in range(5): sock.send_multipart(['', '[%s] hello world' % datetime.now()])
-        for i in range(5): print sock.recv_multipart()[1:]
+        json_data = map(jsonapi.dumps, ['%s' % datetime.now(), 'get_job', None, self.config])
+        sock.send_multipart(json_data)
+        print datetime.now(), map(jsonapi.loads, sock.recv_multipart())
 
 
 def parse_args():
@@ -26,6 +31,10 @@ def parse_args():
 
     parser = ArgumentParser(description='''Worker demo''')
     parser.add_argument(nargs=1, dest='master_uri', type=str)
+    parser.add_argument(nargs='?', dest='time_limit', default='5m')
+    parser.add_argument(nargs='?', dest='memory_limit', default='1G')
+    parser.add_argument(nargs='?', dest='n_procs', type=int, default=1)
+    parser.add_argument(nargs='?', dest='n_threads', type=int, default=1)
     args = parser.parse_args()
     args.master_uri = args.master_uri[0]
     return args
@@ -33,5 +42,7 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
-    w = Worker(args.master_uri)
+    w = Worker(args.master_uri, time_limit=args.time_limit,
+               memory_limit=args.memory_limit, n_procs=args.n_procs,
+               n_threads=args.n_threads)
     w.run()

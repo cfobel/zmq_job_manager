5af8059d17c942df7ec87239cb01da58134823f8
Christian Fobel <christian@fobel.net>
2013-04-03 07:08:57 -0400

    Rearrange order of Broker methods

--- zmq_job_manager/broker.py
+++ zmq_job_manager/broker.py
@@ -38,61 +38,6 @@ class Broker(ZmqJsonRpcTask):
         self._data = None
         super(Broker, self).__init__(on_run=self.on_run, **kwargs)
 
-    def timer__grim_reaper(self, env, *args, **kwargs):
-        for workers_set in ('pending_terminate', 'flatlined'):
-            for uuid, worker in self._data['workers'][workers_set].iteritems():
-                self.terminate_worker(env, uuid)
-
-    def terminate_worker(self, env, worker_uuid):
-        '''
-        Terminate worker externally (i.e., workers that are no longer
-        responding), since we cannot worker would not respond to a request to
-        shutdown gracefully.
-
-        By default, nothing is done except removing the worker from the
-        `pending_terminate` set, but the sub-classes may implement
-        `terminate_worker(self, env, uuid)` accordingly.
-        '''
-        z = ZmqJsonRpcProxy(self._uris['dealer'], uuid=worker_uuid)
-        if worker_uuid in self._data['workers']['pending_terminate']:
-            del self._data['workers']['pending_terminate'][worker_uuid]
-            z.terminated_worker()
-        if worker_uuid in self._data['workers']['flatlined']:
-            z.flatlined_worker()
-
-    def timer__monitor_heartbeats(self, *args, **kwargs):
-        for uuid, worker in self._data['worker_states'].iteritems():
-            heartbeat_count = worker._heartbeat_count
-            if heartbeat_count is not None:
-                # If `heartbeat_count` is `None`, the heart-beat has not been
-                # started.  We only process the heart-beat after it has been
-                # started.
-                heartbeat_count -= 1
-                if heartbeat_count <= 0:
-                    # This process has missed the maximum number of expected
-                    # heartbeats, so add to list of flatlined workers.
-                    self._data['workers']['flatlined'][uuid] = worker
-                    if heartbeat_count == 0:
-                        logging.getLogger(log_label(self)).info(
-                            'worker has flatlined (i.e., heartbeat_count=%d):'
-                            ' %s' % (heartbeat_count, uuid)
-                        )
-                worker._heartbeat_count = heartbeat_count
-
-        for uuid, worker in self._data['workers']['flatlined'].iteritems():
-            if worker._heartbeat_count is not None\
-                    and worker._heartbeat_count > 0:
-                # A worker has come back to life!  Update the worker mappings
-                # accordingly.
-                self._data['workers']['running'][uuid] = worker
-                del self._data['workers']['flatlined'][uuid]
-                logging.getLogger(log_label(self)).info(
-                    'worker %s has revived - heartbeat_count=%d'
-                    % (uuid, heartbeat_count)
-                )
-                z = ZmqJsonRpcProxy(self._uris['dealer'], uuid=uuid)
-                z.revived_worker()
-
     def _initial_data(self):
         '''
         By initializing data using a method, the broker may be sub-classed to,
@@ -137,13 +82,6 @@ class Broker(ZmqJsonRpcTask):
         callback = PeriodicCallback(f, 5000, io_loop=io_loop)
         callback.start()
 
-    def on__heartbeat(self, env, multipart_message, uuid):
-        '''
-        Do nothing here, since heart-beat count is reset in the `call_handler`
-        method.
-        '''
-        return True
-
     def on__available_handlers(self, env, *args, **kwargs):
         return sorted(
             set(self.handler_methods.keys()).union(self._remote_handlers)
@@ -156,6 +94,28 @@ class Broker(ZmqJsonRpcTask):
         z = ZmqJsonRpcProxy(self._uris['dealer'], uuid=uuid)
         result = z.begin_task(task_uuid, **kwargs)
         return result
+
+    def on__broker_hello_world(self, env, multipart_message, uuid):
+        # We received a request on the router socket, so we need to forward
+        # the message to the master through the dealer socket.
+        message = '[%s] hello world (%s)' % (datetime.now(), uuid)
+        logging.getLogger(log_label(self)).info(message)
+        return message
+
+    def on__heartbeat(self, env, multipart_message, uuid):
+        '''
+        Do nothing here, since heart-beat count is reset in the `call_handler`
+        method.
+        '''
+        return True
+
+    def call_handler(self, handler, env, multipart_message, request):
+        # Add `multipart_message` argument to handler call.
+        sender_uuid = request['sender_uuid']
+        if sender_uuid in self._data['worker_states']:
+            self._data['worker_states'][sender_uuid].reset_heartbeat()
+        return handler(env, multipart_message, request['sender_uuid'],
+                    *request['args'], **request['kwargs'])
 
     def get_handler(self, command):
         '''
@@ -172,38 +132,15 @@ class Broker(ZmqJsonRpcTask):
             self.refresh_handler_methods()
         return self._handler_methods.get(command)
 
-    def call_handler(self, handler, env, multipart_message, request):
-        # Add `multipart_message` argument to handler call.
-        sender_uuid = request['sender_uuid']
-        if sender_uuid in self._data['worker_states']:
-            self._data['worker_states'][sender_uuid].reset_heartbeat()
-        return handler(env, multipart_message, request['sender_uuid'],
-                    *request['args'], **request['kwargs'])
-
-    def unpack_request(self, multipart_message):
-        # We need to manually extract the first two frames from the
-        # multipart-message since we are using a `ROUTER` socket for the RPC
-        # socket.  This means that the incoming message has two frames
-        # prepended to the front of the message (compared to when using a `REP`
-        # socket).  Note that these two frames must NOT be mangled in any way,
-        # including de-serializing.
-        #
-        # The first frame contains the ZMQ address of the sender and the second
-        # frame is empty.  These frames are used to keep track of where to send
-        # the message back to when we receive a response on the `DEALER`
-        # socket.
-        message = multipart_message[2:]
-        return super(Broker, self).unpack_request(message)
-
-    def get_uris(self):
-        return self._uris
-
     def get_sock_configs(self):
         return OrderedDict([
                 ('rpc', DeferredSocket(zmq.ROUTER)
                             .stream_callback('on_recv',
                                              self.process_rpc_request)),
         ])
+
+    def get_uris(self):
+        return self._uris
 
     def process_dealer_response(self, env, multipart_message):
         # We received a response to the dealer socket, so we need to forward
@@ -230,13 +167,6 @@ class Broker(ZmqJsonRpcTask):
                 'request: uuid=%(sender_uuid)s command=%(command)s' % request)
         env['socks'][self.rpc_sock_name].send_multipart(data)
 
-    def on__broker_hello_world(self, env, multipart_message, uuid):
-        # We received a request on the router socket, so we need to forward
-        # the message to the master through the dealer socket.
-        message = '[%s] hello world (%s)' % (datetime.now(), uuid)
-        logging.getLogger(log_label(self)).info(message)
-        return message
-
     def send_response(self, socks, multipart_message, request, response):
         # Ignore first element (sender uuid)
         data = map(self.serialize_frame, request.values()[1:])
@@ -252,6 +182,76 @@ class Broker(ZmqJsonRpcTask):
         logging.getLogger(log_label(self)).info(
                 'request: uuid=%(sender_uuid)s command=%(command)s' % request)
         socks[self.rpc_sock_name].send_multipart(data)
+
+    def terminate_worker(self, env, worker_uuid):
+        '''
+        Terminate worker externally (i.e., workers that are no longer
+        responding), since we cannot worker would not respond to a request to
+        shutdown gracefully.
+
+        By default, nothing is done except removing the worker from the
+        `pending_terminate` set, but the sub-classes may implement
+        `terminate_worker(self, env, uuid)` accordingly.
+        '''
+        z = ZmqJsonRpcProxy(self._uris['dealer'], uuid=worker_uuid)
+        if worker_uuid in self._data['workers']['pending_terminate']:
+            del self._data['workers']['pending_terminate'][worker_uuid]
+            z.terminated_worker()
+        if worker_uuid in self._data['workers']['flatlined']:
+            z.flatlined_worker()
+
+    def timer__grim_reaper(self, env, *args, **kwargs):
+        for workers_set in ('pending_terminate', 'flatlined'):
+            for uuid, worker in self._data['workers'][workers_set].iteritems():
+                self.terminate_worker(env, uuid)
+
+    def timer__monitor_heartbeats(self, *args, **kwargs):
+        for uuid, worker in self._data['worker_states'].iteritems():
+            heartbeat_count = worker._heartbeat_count
+            if heartbeat_count is not None:
+                # If `heartbeat_count` is `None`, the heart-beat has not been
+                # started.  We only process the heart-beat after it has been
+                # started.
+                heartbeat_count -= 1
+                if heartbeat_count <= 0:
+                    # This process has missed the maximum number of expected
+                    # heartbeats, so add to list of flatlined workers.
+                    self._data['workers']['flatlined'][uuid] = worker
+                    if heartbeat_count == 0:
+                        logging.getLogger(log_label(self)).info(
+                            'worker has flatlined (i.e., heartbeat_count=%d):'
+                            ' %s' % (heartbeat_count, uuid)
+                        )
+                worker._heartbeat_count = heartbeat_count
+
+        for uuid, worker in self._data['workers']['flatlined'].iteritems():
+            if worker._heartbeat_count is not None\
+                    and worker._heartbeat_count > 0:
+                # A worker has come back to life!  Update the worker mappings
+                # accordingly.
+                self._data['workers']['running'][uuid] = worker
+                del self._data['workers']['flatlined'][uuid]
+                logging.getLogger(log_label(self)).info(
+                    'worker %s has revived - heartbeat_count=%d'
+                    % (uuid, heartbeat_count)
+                )
+                z = ZmqJsonRpcProxy(self._uris['dealer'], uuid=uuid)
+                z.revived_worker()
+
+    def unpack_request(self, multipart_message):
+        # We need to manually extract the first two frames from the
+        # multipart-message since we are using a `ROUTER` socket for the RPC
+        # socket.  This means that the incoming message has two frames
+        # prepended to the front of the message (compared to when using a `REP`
+        # socket).  Note that these two frames must NOT be mangled in any way,
+        # including de-serializing.
+        #
+        # The first frame contains the ZMQ address of the sender and the second
+        # frame is empty.  These frames are used to keep track of where to send
+        # the message back to when we receive a response on the `DEALER`
+        # socket.
+        message = multipart_message[2:]
+        return super(Broker, self).unpack_request(message)
 
 
 def parse_args():

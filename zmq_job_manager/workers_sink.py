5dcdb00da3f3219c08bb657f5eab7348230ba44a
Christian Fobel <christian@fobel.net>
2013-05-06 12:01:34 -0400

    [NB] Refactor to use DurusStorage backend
    Refactor to use `persistent_helpers.storage.DurusStorage` backend.

--- zmq_job_manager/workers_sink.py
+++ zmq_job_manager/workers_sink.py
@@ -1,23 +1,21 @@
 import logging
 from collections import OrderedDict
 from datetime import datetime
-import time
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
 
-import transaction
 import zmq
 from zmq.utils import jsonapi
 from zmq_helpers.socket_configs import DeferredSocket
 from zmq_helpers.rpc import ZmqRpcTask
 from zmq_helpers.utils import log_label
-from path import path
+from persistent_helpers.storage import DurusStorage
 from persistent_helpers.ordered_dict import PersistentOrderedDict
-from ZODB import DB
-from ZEO import ClientStorage
-from BTrees.OOBTree import OOBTree
+from durus.persistent_set import PersistentSet
+import durus.btree
+from path import path
 
 
 class SubMessage(object):
@@ -44,80 +42,22 @@ class SubMessage(object):
 
 class WorkersSink(ZmqRpcTask):
     default_data_dir = path('~/.workers_sink').expand()
-    default_db_name = 'zeo.socket'
 
-    def __init__(self, rpc_uri, sub_uri, data_dir=None, db_name=None,
+    def __init__(self, rpc_uri, sub_uri, storage, data_dir=None,
                  init_subcribe=None, **kwargs):
         self._uris = OrderedDict()
         self._uris['rpc'] = rpc_uri
         self._uris['sub'] = sub_uri
         self._registered_workers = set()
         self._init_subscribe = init_subcribe
-        if db_name is None:
-            self._db_name = self.default_db_name
-        else:
-            self._db_name = db_name
+        self.storage = storage
         if data_dir is None:
             self._data_dir = self.default_data_dir
         else:
             self._data_dir = path(data_dir).abspath()
         if not self._data_dir.isdir():
             self._data_dir.makedirs_p()
-        super(WorkersSink, self).__init__(on_run=self.on_run, sub_bind=False,
-                                          **kwargs)
-
-    def on_run(self, ctx, io_loop, socks, streams):
-        self._db_path = self._data_dir.joinpath(self._db_name)
-        logging.getLogger(log_label(self)).info('using db path: %s',
-                                                self._db_path)
-        self._reset_db()
-
-    def _reset_db(self):
-        self._storage = ClientStorage.ClientStorage(self._db_path)
-        self._db = DB(self._storage)
-        self._connection = self._db.open()
-        self._root = self._connection.root()
-
-    def cleanup(self):
-        for k in ('_connection', '_db', '_storage', ):
-            v = getattr(self, k, None)
-            if v:
-                v.close()
-
-    def __del__(self):
-        self.cleanup()
-
-    def _get_db_node(self, db_path):
-        nodes = db_path.split('/')
-        assert(nodes[0] == '')
-        node_names = nodes[1:]
-        for i in range(3):
-            try:
-                node = self._root
-                for i in range(len(node_names)):
-                    if node_names[i] not in node:
-                        node[node_names[i]] = PersistentOrderedDict()
-                    node = node[node_names[i]]
-                return node
-            except:
-                self._reset_db()
-                time.sleep(0.1)
-                return self._get_db_node(db_path)
-        raise
-
-    def _save_to_db(self, db_path, value):
-        for i in range(3):
-            try:
-                db_path = path(db_path)
-                node = self._get_db_node(str(db_path.parent))
-                node[str(db_path.name)] = value
-                transaction.commit()
-                return node
-            except:
-                self._reset_db()
-                time.sleep(0.1)
-                return self._save_to_db(db_path, value)
-        raise
+        super(WorkersSink, self).__init__(sub_bind=False, **kwargs)
 
     def get_uris(self):
         return self._uris
@@ -175,27 +115,39 @@ class WorkersSink(ZmqRpcTask):
         logging.getLogger(log_label(self)).info(data)
 
     def save(self, worker_uuid, task_uuid, key, value):
-        logging.getLogger(log_label(self)).info(self._root.keys())
-        db_path = '/data/%s/%s/%s' % (worker_uuid, task_uuid, key)
-        parent_node = self._save_to_db(db_path, value)
-        if 'task_by_uuid' not in self._root or task_uuid not in self._root['task_by_uuid']:
-            db_path = '/task_by_uuid/%s' % task_uuid
-            self._save_to_db(db_path, parent_node)
-            # Since we are referencing the same data item multiple times,
-            # if we do not conduct a `pack` of the database, the file size will
-            # grow according to a multiple of the database size.
-            #self._db.pack()
-        if 'datetime_by_task_uuid' not in self._root\
-                or task_uuid not in self._root['datetime_by_task_uuid']:
-            db_path = '/datetime_by_task_uuid/%s' % task_uuid
-            now = datetime.now()
-            self._save_to_db(db_path, now)
-            #self._db.pack()
-            task_uuid_by_datetime = self._root.setdefault('task_uuid_by_datetime', OOBTree())
-            task_uuid_by_datetime[now] = task_uuid
-            transaction.commit()
-            #self._db.pack()
-        logging.getLogger(log_label(self)).info(self._root.keys())
+        logging.getLogger(log_label(self)).info(self.storage.root.keys())
+        for i in range(3):
+            self.storage.abort()
+            try:
+                db_path = '/worker_uuid_by_task_uuid/%s' % task_uuid
+                self.storage[db_path] = value
+                db_path = '/task_by_uuid/%s/%s' % (task_uuid, key)
+                self.storage[db_path] = value
+                if task_uuid not in self.storage.root.setdefault(
+                        'datetime_by_task_uuid', PersistentOrderedDict()):
+                    db_path = '/datetime_by_task_uuid/%s' % task_uuid
+                    now = datetime.now()
+                    self.storage[db_path] = now
+                    task_uuid_by_datetime = self.storage.root.setdefault(
+                            'task_uuid_by_datetime', durus.btree.BTree())
+                    task_uuid_by_datetime[now] = task_uuid
+                task_uuids_by_label = self.storage.root.setdefault(
+                        'task_uuids_by_label', PersistentOrderedDict())
+                if key == '__label__':
+                    logging.getLogger(log_label(self)).warning('key=%s value=%s', key, value)
+                    label = value
+                    uuids = task_uuids_by_label.setdefault(label, PersistentSet())
+                    if task_uuid not in uuids:
+                        logging.getLogger(log_label(self)).warning(
+                                'adding %s to task_uuids_by_label["%s"]',
+                                task_uuid, label)
+                        uuids.add(task_uuid)
+                        self.storage.root._p_note_change()
+                self.storage.commit()
+                return
+            except:
+                pass
+        raise
 
     def sub__store(self, message, serialization, key, value):
         if serialization == 'SERIALIZE__PICKLE':
@@ -203,7 +155,7 @@ class WorkersSink(ZmqRpcTask):
         elif serialization == 'SERIALIZE__JSON':
             value = jsonapi.loads(value)
         self.save(message.worker_uuid, message.task_uuid, key, value)
-        logging.getLogger(log_label(self)).info('key=%s value=%s', key, value)
+        logging.getLogger(log_label(self)).debug('key=%s value=%s', key, value)
 
     def process_sub_message(self, env, multipart_message):
         message = SubMessage.from_multipart_message(multipart_message)
@@ -251,5 +203,6 @@ by the date and time the first message was received from each worker.
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     args = parse_args()
-    b = WorkersSink(args.rpc_uri, args.sub_uri, init_subcribe='')
+    b = WorkersSink(args.rpc_uri, args.sub_uri, storage=DurusStorage(),
+                    init_subcribe='')
     b.run()

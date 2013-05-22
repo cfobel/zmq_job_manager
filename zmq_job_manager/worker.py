bf9a7f7e2cd96f1c3248c7888833b042b7024f79
Christian Fobel <christian@fobel.net>
2013-05-22 10:52:31 -0400

    Fix worker logging

--- zmq_job_manager/worker.py
+++ zmq_job_manager/worker.py
@@ -408,6 +408,14 @@ def parse_args():
 
 
 if __name__ == '__main__':
+    # if someone tried to log something before basicConfig is called, Python
+    # creates a default handler that
+    # goes to the console and will ignore further basicConfig calls. Remove the
+    # handler if there is one.
+    root = logging.getLogger()
+    if root.handlers:
+        for handler in root.handlers:
+            root.removeHandler(handler)
     logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s-%(name)s-%(levelname)s:%(message)s")
     args = parse_args()

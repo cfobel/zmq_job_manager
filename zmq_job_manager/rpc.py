be2f3fbbf3f7b763493d3e344451c801fd8841b6
Christian Fobel <christian@fobel.net>
2013-05-24 00:31:13 -0400

    Refactor queue code into DeferredZmqRpcQueue

---
+++ zmq_job_manager/rpc.py
@@ -0,0 +1,109 @@
+from collections import OrderedDict
+from uuid import uuid4
+
+from zmq_helpers.rpc import ZmqRpcProxy
+import eventlet
+
+
+class DeferredZmqRpcQueue(object):
+    def __init__(self, rpc_uri, queue_storage=None, uuid=None):
+        self.uris = OrderedDict(rpc=rpc_uri)
+
+        if uuid is None:
+            self.uuid = str(uuid4())
+        else:
+            self.uuid = uuid
+        self.proxy = ZmqRpcProxy(self.uris['rpc'], uuid=self.uuid)
+        if queue_storage is None:
+            queue_storage = []
+        self.request_queue = queue_storage
+        self._deferred_request = None
+
+    def queue_request(self, command, *args, **kwargs):
+        '''
+        Queue request to be executed once all prior pending requests have been
+        processed.
+        '''
+        self.request_queue.append((command, args, kwargs))
+
+    @property
+    def request_pending(self):
+        '''
+        Return whether or not there is a spawned request pending.
+        '''
+        return (self._deferred_request is not None)
+
+    @property
+    def queue_length(self):
+        '''
+        Return the length of the current request queue.
+        '''
+        return len(self.request_queue)
+
+    @property
+    def queue(self):
+        '''
+        Return the current request queue contents.
+        '''
+        return self.request_queue
+
+    def process_queue_item(self):
+        '''
+        Process the least-recently queued request by spawning a new deferred
+        request instance and assigning it to the `_deferred_request` attribute.
+        If there are no queued requests, return `None`.  Otherwise, return
+        `True`.
+
+        Raise an exception if there is already an active/pending spawned
+        request.
+        '''
+        if not self.request_queue:
+            return None
+        elif self._deferred_request is not None:
+            raise RuntimeError, ('Previous deferred request must be completed '
+                                 'before processing the next deferred request.')
+
+        command, args, kwargs = self.request_queue[0]
+        f = getattr(self.proxy, command)
+        self._deferred_request = f.spawn(*args, **kwargs)
+        return True
+
+    def __iter__(self):
+        while self.queue_length > 0:
+            yield self.next_result()
+
+    def next_result(self):
+        '''
+        Process next item in queue and block until result is ready.  Return
+        result.
+        '''
+        if (self._deferred_request is not None or
+                self.process_queue_item()):
+            while not self.ready():
+                eventlet.sleep(0.001)
+            return self.wait()
+        raise ValueError, 'There are no pending requests.'
+
+    def ready(self):
+        '''
+        Return the status of the current active/pending spawned request.
+        '''
+        return self._deferred_request.ready()
+
+    def wait(self):
+        '''
+        Return the result from the current spawned request.
+
+        Raise an exception if the spawned request is not ready, or there is no
+        pending spawned request.
+        '''
+        if self._deferred_request is None:
+            raise RuntimeError, 'No pending deferred request.'
+        elif not self._deferred_request.ready():
+            raise RuntimeError, 'Deferred must be ready before calling wait'
+
+        result = self._deferred_request.wait()
+        del self.request_queue[0]
+        del self._deferred_request
+        self._deferred_request = None
+        return result
--- zmq_job_manager/worker_test.py
+++ zmq_job_manager/worker_test.py
@@ -1,44 +1,26 @@
-import sys
-import traceback
-import threading
 import signal
 import functools
-from multiprocessing import Pipe
-from datetime import datetime, timedelta
-from threading import Thread
 import logging
 from collections import OrderedDict
 from uuid import uuid4
 import time
-import platform
-import os
+from datetime import datetime
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
 
-import psutil
-import netifaces
 import eventlet
-import zmq
+from zmq.eventloop.ioloop import PeriodicCallback
 # The following durus imports cause the worker process to hang
 # (see #4, https://github.com/cfobel/zmq_job_manager/issues/4)
 # For now, the worker process throws a `SystemError` exception when terminating
 # to force termination.
-from durus.persistent_list import PersistentList
-from persistent_helpers.storage import DurusStorage
-from zmq.utils import jsonapi
-from zmq.eventloop import zmqstream
-from zmq.eventloop.ioloop import IOLoop, PeriodicCallback
 from zmq_helpers.socket_configs import get_run_context
-from zmq_helpers.rpc import ZmqRpcTask, ZmqRpcProxy, RpcHandlerMixin
-from zmq_helpers.utils import log_label, unique_ipc_uri, cleanup_ipc_uris
-from cpu_info.cpu_info import cpu_info, cpu_summary
+from zmq_helpers.rpc import ZmqRpcTask
+from zmq_helpers.utils import log_label
 
-from .manager import get_seconds_since_epoch
-from .process import PopenPipeReactor
-from .constants import SERIALIZE__PICKLE, SERIALIZE__JSON
-from .worker_limits import extract_memory_limit, extract_runtime_limit
+from .rpc import DeferredZmqRpcQueue
 
 
 class WorkerTest(ZmqRpcTask):
@@ -51,95 +33,39 @@ class WorkerTest(ZmqRpcTask):
             self.uuid = str(uuid4())
         else:
             self.uuid = uuid
-        self.supervisor = ZmqRpcProxy(self.uris['supervisor'], uuid=self.uuid)
-        self.storage = DurusStorage(host='%s.durus.dat' % self.uuid,
-                                    port=False)
-        if queue_storage is None:
-            queue_storage = []
-        self.request_queue = queue_storage
-        self._deferred_request = None
+        self.deferred_queue = DeferredZmqRpcQueue(supervisor_uri, uuid=uuid)
 
     def get_uris(self):
         return self.uris
 
-    def rpc__queue_request(self, env, client_uuid, command, *args, **kwargs):
-        '''
-        Queue request to be executed once all prior pending requests have been
-        processed.
-        '''
-        self.request_queue.append((command, args, kwargs))
+    def rpc__queue_request(self, env, client_uuid, *args, **kwargs):
+        return self.deferred_queue.queue_request(*args, **kwargs)
 
     def rpc__get_queue_length(self, env, client_uuid):
-        '''
-        Return the length of the current request queue.
-        '''
-        return len(self.request_queue)
+        return self.deferred_queue.queue_length
 
     def rpc__get_queue(self, env, client_uuid):
-        '''
-        Return the current request queue contents.
-        '''
-        return self.request_queue
+        return self.deferred_queue.queue
 
     def rpc__process_queue_item(self, env, client_uuid):
-        '''
-        Process the least-recently queued request by spawning a new deferred
-        request instance and assigning it to the `_deferred_request` attribute.
-        If there are no queued requests, return `None`.  Otherwise, return
-        `True`.
-
-        Raise an exception if there is already an active/pending spawned
-        request.
-        '''
-        if not self.request_queue:
-            return None
-        elif self._deferred_request is not None:
-            raise RuntimeError, ('Previous deferred request must be completed '
-                                 'before processing the next deferred request.')
-
-        command, args, kwargs = self.request_queue[0]
-        f = getattr(self.supervisor, command)
-        self._deferred_request = f.spawn(*args, **kwargs)
-        return True
+        return self.deferred_queue.process_queue_item()
 
     def rpc__next_result(self, env, client_uuid):
-        '''
-        Process next item in queue and block until result is ready.  Return
-        result.
-        '''
-        if (self._deferred_request is not None or
-                self.rpc__process_queue_item(env, client_uuid)):
-            while not self.rpc__deferred_ready(env, client_uuid):
-                eventlet.sleep(0.001)
-            return self.rpc__deferred_wait(env, client_uuid)
-        raise ValueError, 'There are no pending requests.'
+        return self.deferred_queue.next_result()
 
     def rpc__deferred_ready(self, env, client_uuid):
-        '''
-        Return the status of the current active/pending spawned request.
-        '''
-        return self._deferred_request.ready()
+        return self.deferred_queue.ready()
 
     def rpc__deferred_wait(self, env, client_uuid):
-        '''
-        Return the result from the current spawned request.
-
-        Raise an exception if the spawned request is not ready, or there is no
-        pending spawned request.
-        '''
-        if self._deferred_request is None:
-            raise RuntimeError, 'No pending deferred request.'
-        elif not self._deferred_request.ready():
-            raise RuntimeError, 'Deferred must be ready before calling wait'
-
-        result = self._deferred_request.wait()
-        del self.request_queue[0]
-        del self._deferred_request
-        self._deferred_request = None
-        return result
+        return self.deferred_queue.wait()
 
     def timer__queue_monitor(self, io_loop):
-        pass
+        if self.deferred_queue.queue_length > 0:
+            if not self.deferred_queue.request_pending:
+                self.deferred_queue.process_queue_item()
+            elif self.deferred_queue.ready():
+                result = self.deferred_queue.wait()
+                print result
 
     def handle_sigterm(self, io_loop):
         io_loop.stop()
@@ -154,7 +80,7 @@ class WorkerTest(ZmqRpcTask):
         # Periodically send a heartbeat signal to let the supervisor know we're
         # still running.
         callbacks['queue_monitor'] = PeriodicCallback(
-            functools.partial(self.timer__queue_monitor, io_loop), 1000,
+            functools.partial(self.timer__queue_monitor, io_loop), 10,
             io_loop=io_loop)
 
         callbacks['event_sleep'] = PeriodicCallback(lambda: eventlet.sleep(),
